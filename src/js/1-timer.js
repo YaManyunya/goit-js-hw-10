8fa9204ed73c415241452149c43146edb20d8f1c
Nadiya <yakovlevanadya777@gmail.com>
2026-02-19 21:33:31 +0200

    1_1

---
+++ src/js/1-timer.js
@@ -0,0 +1,83 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const datetimePicker = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("[data-start]");
+const timerVeiw = document.querySelectorAll(".value");
+
+startBtn.addEventListener("click", startTimer);
+
+let userSelectedDate = null;
+let timerId = null;
+
+// Налаштування flatpickr
+flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate <= new Date()) {
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+        messageColor: "#fff",
+        backgroundColor: "#ef4040",
+        position: "center"
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+      userSelectedDate = selectedDates[0]; 
+    }
+  },
+});
+
+function startTimer(){
+    datetimePicker.disabled = true;
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+       const currentTime = Date.now();
+       const result = userSelectedDate - currentTime;
+        if (result <= 0){ 
+           datetimePicker.disabled = false;
+           clearInterval(timerId); 
+           updateTimer({ days: "00", hours: "00", minutes: "00", seconds: "00" });  
+           return;  
+        }; 
+       const ms = convertMs(result);
+       updateTimer(ms);
+
+    },1000);
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value){
+  return String(value).padStart(2, "0");
+};
+
+function updateTimer(obj){
+    const values = Object.values(obj);
+    timerVeiw.forEach((item, index) => {
+    item.textContent = values[index];  
+}); 
+}
